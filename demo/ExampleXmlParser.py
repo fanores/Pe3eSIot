cc6149d1c6e4276bedc2cd02614492ba2d612869
Stefan Misik <fanores@gmail.com>
2021-05-08 21:13:52 +0200

    add demo example for XmlParser

---
+++ demo/ExampleXmlParser.py
@@ -0,0 +1,42 @@
+"""FVE ThingSpeak - Upload"""
+from lib.HttpClient import HttpClient
+from lib.IotError import HttpClientError
+from lib.XmlParser import XmlParser
+from lib.IotError import XmlParserError
+from datetime import datetime
+
+
+def main():
+	# Initialization
+	fve_url = 'http://192.168.2.51:8003/'
+
+	# Trigger the processing
+	process_actual_measurement(fve_url)
+
+
+def process_actual_measurement(fve_url):
+	print('Processing starts at: {}'.format(datetime.now().strftime("%d/%m/%Y %H:%M:%S")))
+
+	print('HTTP Client Processing:')
+	ACTUAL_MEASUREMENT_URL_SUFFIX = 'meas.xml'
+	request_url = fve_url + ACTUAL_MEASUREMENT_URL_SUFFIX
+	http_client = HttpClient()
+	try:
+		fve_response = http_client.get_request(request_url)
+	except HttpClientError as error:
+		print(error)
+
+	print('XML Processing:')
+	xml_parser = XmlParser()
+	try:
+		elements = xml_parser.parse_root_child_elements_into_dictionary(fve_response.text)
+	except XmlParserError as error:
+		print(error)
+
+	print(elements)
+
+	print('Processing is finished!')
+
+
+if __name__ == '__main__':
+	main()
