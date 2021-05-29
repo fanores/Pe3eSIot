38b51eeade6feb41a4452223f5118cddeda9bc8c
Stefan Misik <fanores@gmail.com>
2021-05-29 23:06:30 +0200

    add unit tests for FileProcessor

---
+++ tests/lib/test_fileProcessor.py
@@ -0,0 +1,37 @@
+import pytest
+import unittest
+from unittest.mock import patch, mock_open
+from lib.FileProcessor import FileProcessor
+from lib.IotError import FileProcessorError
+
+
+class TestFileProcessor:
+
+	def test_write_into_file_successfully(self):
+		with patch('builtins.open', mock_open()) as mocked_file:
+			# GIVEN
+			file_path = 'fake/file/path'
+			content = 'My file content'
+
+			# WHEN
+			file_processor = FileProcessor(file_path)
+			file_processor.append_line_into_file(content)
+
+			# THEN
+			mocked_file.assert_called_once_with(file_path, 'a+')
+			mocked_file().write.assert_called_once_with(content + '\n')
+
+	def test_write_into_file_raises_exception(self):
+		with pytest.raises(FileProcessorError):
+			with patch('builtins.open', mock_open()) as mocked_file:
+				# GIVEN
+				file_path = 'fake/file/path'
+				content = 'My file content'
+				mocked_file.side_effect = FileNotFoundError()
+
+				# WHEN
+				file_processor = FileProcessor(file_path)
+				file_processor.append_line_into_file(content)
+
+				# THEN
+				# FileProcessorError exception was raised
