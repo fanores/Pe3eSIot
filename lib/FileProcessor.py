1f154d9e9edc80c5c97e4ac0844f0a077a4cbd0e
Stefan Misik <fanores@gmail.com>
2021-05-29 22:24:29 +0200

    add simple file processor class

---
+++ lib/FileProcessor.py
@@ -0,0 +1,23 @@
+"""File Processor"""
+from lib.IotError import FileProcessorError
+
+
+class FileProcessor:
+
+    # constructor
+    def __init__(self, file):
+        self.file = file
+
+    # append line into file
+    def append_line_into_file(self, line):
+        """
+            Append Line Into a File
+        """
+
+        try:
+            file = open(str(self.file), 'a+')
+        except FileNotFoundError as error:
+            raise FileProcessorError(print(error))
+
+        file.write(line + "\n")
+        file.close()
