b1fb75ddedde68d533d8ca271704ae35b2f23fb8
Stefan Misik <stefan.misik@Stefans-MacBook-Pro.local>
2022-10-16 21:02:05 +0200

    Remove unnecessary weather methods from weather manager

--- lib/weather/OpenWeatherMapManager.py
+++ lib/weather/OpenWeatherMapManager.py
@@ -10,28 +10,6 @@ class OpenWeatherMapManager:
         self.owm = OWM(api_key)
         self.owm_manager = self.owm.weather_manager()
 
-    def get_city_id_by_city_name(self, city, country):
-        """
-            Get City ID: returns the city ID from city name and country
-        :return: city id
-        """
-        registry = self.owm.city_id_registry()
-        city_list = registry.ids_for(city, country, matching='exact')
-        city_properties = city_list[0]
-
-        return city_properties[0]
-
-    def get_geo_location_by_city_name(self, city, country):
-        """
-            Get City Geo Locations: returns the city GEO Location coordinates
-        :return: geo location coordinates
-        """
-        registry = self.owm.city_id_registry()
-        city_list = registry.locations_for(city, country, matching='exact')
-        city_geo_coordinates = city_list[0]
-
-        return city_geo_coordinates
-
     def get_current_temperature_at_place(self, city, country, unit="celsius"):
         """
             Get Current Temperature at Place: returns current temperature at place specified by city name and country
