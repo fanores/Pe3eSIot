845e02af1fc75cd11ade2c10a314742e74068e55
Stefan Misik <stefan.misik@Stefans-MacBook-Pro.local>
2022-10-22 11:05:56 +0200

    Add exception handling for OWM manager

--- lib/weather/OpenWeatherMapManager.py
+++ lib/weather/OpenWeatherMapManager.py
@@ -7,16 +7,22 @@ class OpenWeatherMapManager:
 
     # constructor
     def __init__(self, api_key):
-        self.owm = OWM(api_key)
-        self.owm_manager = self.owm.weather_manager()
+        try:
+            self.owm = OWM(api_key)
+            self.owm_manager = self.owm.weather_manager()
+        except AssertionError as error:
+            raise OwmManagerError('OWM Error: {}'.format(error))
 
     def get_current_temperature_at_place(self, city, country, unit="celsius"):
         """
             Get Current Temperature at Place: returns current temperature at place specified by city name and country
         :return: current temperature at place
         """
-        place = city + ',' + country
-        current_weather = self.owm_manager.weather_at_place(place).weather
-        current_temperature = current_weather.temperature(unit)
+        try:
+            place = city + ',' + country
+            current_weather = self.owm_manager.weather_at_place(place).weather
+            current_temperature = current_weather.temperature(unit)
+        except Exception as error:
+            raise OwmManagerError('OWM Error: {}'.format(error))
 
         return current_temperature + unit
