72402d8f4751b0a04c2f3bf6cda7f1d0ac8b0bfa
Stefan Misik <stefan.misik@Stefans-MacBook-Pro.local>
2022-10-22 11:06:23 +0200

    Add unit tests for OWM exception handling

--- tests/lib/weather/test_openWeatherMapManager.py
+++ tests/lib/weather/test_openWeatherMapManager.py
@@ -12,11 +12,15 @@ class FakeWeather(object):
 
 class FakeWeatherManager(object):
 
-	def __init__(self):
+	def __init__(self, raise_owm_manager_exception):
 		self.place = ''
 		self.weather = ''
+		self.raise_exception = raise_owm_manager_exception
 
 	def weather_at_place(self, place):
+		if self.raise_exception:
+			raise Exception('Error in OWM')
+
 		self.place = place
 		self.weather = FakeWeather()
 		return self
@@ -24,17 +28,18 @@ class FakeWeatherManager(object):
 
 class FakeOwm(object):
 
-	def __init__(self, api_key):
+	def __init__(self, api_key, raise_owm_manager_exception):
 		self.api_key = api_key
 		self.weather_manager_called = False
+		self.raise_owm_manager_exception = raise_owm_manager_exception
 
 	def weather_manager(self):
 		self.weather_manager_called = True
-		return FakeWeatherManager()
+		return FakeWeatherManager(self.raise_owm_manager_exception)
 
 
-def mock_owm(api_key):
-	return FakeOwm(api_key)
+def mock_owm(api_key, raise_owm_manager_exception=False):
+	return FakeOwm(api_key, raise_owm_manager_exception)
 
 
 class TestOpenWeatherMapManager:
@@ -77,3 +82,25 @@ class TestOpenWeatherMapManager:
 
 		# THEN
 		assert current_temperature == '9farenheit'
+
+	@patch('lib.weather.OpenWeatherMapManager.OWM')
+	def test_create_open_weather_map_manager_exception(self, owm_patch):
+		with pytest.raises(OwmManagerError, match=r'.*OWM Error.*'):
+			# GIVEN
+			fake_api_key = None
+			owm_patch.side_effect = AssertionError('Error in OWM')
+
+			# WHEN
+			owm_manager = OpenWeatherMapManager(fake_api_key)
+
+	@patch('lib.weather.OpenWeatherMapManager.OWM')
+	def test_get_current_temperature_at_place_exception(self, owm_patch):
+		with pytest.raises(OwmManagerError, match=r'.*OWM Error.*'):
+			# GIVEN
+			fake_api_key = '1234'
+			raise_weather_manager_exception = True
+			owm_patch.return_value = mock_owm(fake_api_key, raise_weather_manager_exception)
+
+			# WHEN
+			owm_manager = OpenWeatherMapManager(fake_api_key)
+			current_temperature = owm_manager.get_current_temperature_at_place('Nemsova', 'SK')
